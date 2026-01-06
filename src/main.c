000c192bbd1ca58cc0ebbd4e6741703332fd4f33
zengzhi <zengzhi@we-wins.com>
2026-01-06 17:25:37 +0800

    //最低功耗增加串口挂起的控制

--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2012-2014 Wind River Systems, Inc.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
+* Copyright (c) 2012-2014 Wind River Systems, Inc.
+*
+* SPDX-License-Identifier: Apache-2.0
+*/
 
 #include <stdio.h>
 #include <zephyr/kernel.h>
@@ -11,20 +11,29 @@
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
 #include <zephyr/pm/device.h>
-const struct device *uart = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
 #include "npm_fuel_gauge.h"
 
+
 #define GPIO2_NODE DT_NODELABEL(gpio2)
+const struct device *uart = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
+const struct device *uart54 = DEVICE_DT_GET(DT_NODELABEL(uart22));
+const struct device *gpio2_dev = DEVICE_DT_GET(GPIO2_NODE);
+
 
 #define AG3352_VCORE_VRF_DCDC_EN 	3
 #define AG3352_VRTC_LOAD_SW_EN 		4
 #define AG3352_AVDD_VSYS_PWR_CTRL 	6
 #define N91_ENABLE 					10
 
+
 int main(void)
 {
-	const struct device *gpio2_dev = DEVICE_DT_GET(GPIO2_NODE);
+	if (!device_is_ready(uart54)) {
+		printf("UART device not ready\n");
+		return 0;
+	}
+
 	gpio_pin_configure(gpio2_dev, AG3352_VCORE_VRF_DCDC_EN, GPIO_OUTPUT_LOW);
 	gpio_pin_configure(gpio2_dev, AG3352_VRTC_LOAD_SW_EN, GPIO_OUTPUT_LOW);
 	gpio_pin_configure(gpio2_dev, AG3352_AVDD_VSYS_PWR_CTRL, GPIO_OUTPUT_LOW);
@@ -34,7 +43,9 @@ int main(void)
 	gpio_pin_configure(gpio2_dev, N91_ENABLE, GPIO_OUTPUT_LOW);
 	#endif
 	npm_initial();
+
 	pm_device_action_run(uart, PM_DEVICE_ACTION_SUSPEND);
+	pm_device_action_run(uart54, PM_DEVICE_ACTION_SUSPEND);
 
 	for (;;)
 	{
